a6656a9d548de0e2aa7762e2188013407327cb2f
gusholguin <augustin.holguin@gmail.com>
2018-01-05 19:37:52 +0100

    chapter 8 done: Vue CLI

---
+++ VueJS/src/Counter.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="counter">
+    {{ count }}
+    <div class="counter__button" @click="increment">+</div>
+  </div>
+</template>
+
+
+<script>
+  import store from './store/NotificationsStore'
+
+  export default {
+    data () {
+      return {
+        state: store.state
+      }
+    },
+    computed: {
+      count: function () { return this.state.count }
+    },
+    methods: {
+      increment () {
+        store.increment()
+      }
+    }
+  }
+</script>
+
+<style>
+
+  .counter {
+    font-weight: bold;
+    font-size: 2em;
+    color: gold;
+  }
+
+  .counter__button {
+    cursor: pointer;
+    color: blue;
+  }
+</style>
