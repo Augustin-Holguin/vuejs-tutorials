1fe5f76c03170c03452ebf173ccb7c13a1dacca7
gusholguin <augustin.holguin@gmail.com>
2018-01-08 14:36:32 +0100

    mvc todo app v1

---
+++ Todos/src/components/Todos.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="todoapp">
+    <header>
+      <h1>Todo app</h1>
+      <input type="text" class="new-todo" placeholder="Add a todo">
+    </header>
+    <div class="main">
+      <ul class="todo-list">
+        <li class="todo">
+          <div class="view">
+            <label>Todo name</label>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+
+<script>
+  export default {
+  }
+</script>
+
+
+<style src="./todos.css"></style>
